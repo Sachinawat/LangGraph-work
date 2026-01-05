00b0211f623dbab5ef727b09026fdbad1afd41b6
Sachin Awati <awatisachin021@gmail.com>
2026-01-05 11:27:02 +0530

    added hello world file

---
+++ test1.py
@@ -0,0 +1,17 @@
+import warnings
+warnings.filterwarnings('ignore', category=UserWarning, module='langchain_core._api.deprecation')
+
+from langgraph.graph import StateGraph, MessagesState, START, END
+
+def mock_llm(state: MessagesState):
+    return {"messages": [{"role": "ai", "content": "hello world"}]}
+
+graph = StateGraph(MessagesState)
+graph.add_node(mock_llm)
+graph.add_edge(START, "mock_llm")
+graph.add_edge("mock_llm", END)
+graph = graph.compile()
+
+result = graph.invoke({"messages": [{"role": "user", "content": "hi!"}]})
+print(result)
+
